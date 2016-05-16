6b2b5f389668e1edf10aa364b0cfafb9a294d2a9
ZeroDY <deyi233@gmail.com>
2016-05-17 01:44:22 +0800

    测试 git

--- Shougongke/ViewController.m
+++ Shougongke/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"测试手工客 git 版本1");
 }
 
 - (void)didReceiveMemoryWarning {
